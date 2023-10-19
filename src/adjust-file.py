af0f6fc3fd0b6a0ede19d58d0c606d7367cd2128
Francesco Periti <peritifrancesco@gmail.com>
2023-10-19 15:40:55 +0200

    DURel

--- src/adjust-file.py
+++ src/adjust-file.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     tmp = list()
     with open('TRoTR/judgments/3rd-round.tsv', mode='r', encoding='utf-8') as f:
         tmp.append(f.readline())
-        f = f.read().replace('shur\n', 'shur@@@').replace('Nisha\n', 'Nisha@@@').replace('AndreaMariaC\n',
+        f = f.read().replace('iosakwe\n', 'iosakwe@@@').replace('shur\n', 'shur@@@').replace('Nisha\n', 'Nisha@@@').replace('AndreaMariaC\n',
                                                                                          'AndreaMariaC@@@').replace(
             '\n', '--')
         lines = f.split('@@@')
