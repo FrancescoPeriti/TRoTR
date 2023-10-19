466d6ae277746c60570b64e629ea7af1e1ee00d8
Francesco Periti <peritifrancesco@gmail.com>
2023-10-19 15:55:26 +0200

    adjust file

--- src/adjust-file.py
+++ src/adjust-file.py
@@ -2,10 +2,11 @@ if __name__ == '__main__':
     tmp = list()
     with open('TRoTR/judgments/3rd-round.tsv', mode='r', encoding='utf-8') as f:
         tmp.append(f.readline())
-        f = f.read().replace('iosakwe\n', 'iosakwe@@@').replace('shur\n', 'shur@@@').replace('Nisha\n', 'Nisha@@@').replace('AndreaMariaC\n',
+        f = f.read().replace('shur\n', 'shur@@@').replace('Nisha\n', 'Nisha@@@').replace('AndreaMariaC\n',
                                                                                          'AndreaMariaC@@@').replace(
             '\n', '--')
         lines = f.split('@@@')
+        if len(set(lines[-1])) == 1: lines=lines[:-1]
         tmp.extend([line + '\n' for line in lines])
     with open('TRoTR/judgments/3rd-round.tsv', mode='w', encoding='utf-8') as f:
         f.writelines(tmp)
