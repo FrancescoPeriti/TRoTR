bf8a14847b8cd3b388b1b58bd2e5d5568615113f
Francesco Periti <peritifrancesco@gmail.com>
2023-10-30 14:45:04 +0100

    TRiC model

--- src/cls_head.py
+++ src/cls_head.py
@@ -40,7 +40,7 @@ class CLSModel(BertPreTrainedModel): #PreTrainedModel):
         self._tokenizer = AutoTokenizer.from_pretrained(config._name_or_path)
         self._input_size = config.hidden_size
         self._max_seq_len = config.max_position_embeddings
-        self._clf = ClassificationHead(1, self._input_size)
+        self._clf = ClassificationHead(1, self._input_size * 2) # two embeddings will be concatenated
         self.init_weights()
 
     def forward(self,
