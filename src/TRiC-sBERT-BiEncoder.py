9994691d19dfbc0932f623320ea6266ac0a50cfd
FrancescoPeriti <peritifrancesco@gmail.com>
2023-11-24 11:40:24 +0100

    sBERT update

--- src/TRiC-sBERT-BiEncoder.py
+++ src/TRiC-sBERT-BiEncoder.py
@@ -6,6 +6,7 @@ from sklearn.metrics import roc_curve
 from scipy.spatial.distance import cosine
 from scipy.stats import spearmanr, pearsonr
 from sklearn.metrics import f1_score, roc_curve, recall_score
+from sklearn.metrics import classification_report
 from sentence_transformers import SentenceTransformer
 
 import argparse
@@ -109,8 +110,14 @@ f1_scores_label1 = list()
 mask_f1_scores_label1 = list()
 f1_scores_label0 = list()
 mask_f1_scores_label0 = list()
-recall_scores = list()
-mask_recall_scores = list()
+recall_label1 = list()
+mask_recall_label1 = list()
+recall_label0 = list()
+mask_recall_label0 = list()
+precision_label1 = list()
+mask_precision_label1 = list()
+precision_label0 = list()
+mask_precision_label0 = list()
 
 for data_set in ['train', 'test.iov', 'test.oov', 'dev']:
     f1 = f1_score(labels[data_set], [m <= thr for m in distances[data_set]], average='weighted')
@@ -118,23 +125,40 @@ for data_set in ['train', 'test.iov', 'test.oov', 'dev']:
     f1 = f1_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], average='weighted')
     mask_f1_scores.append(f1)
 
-    f1 = f1_score(labels[data_set], [m <= thr for m in distances[data_set]], average='binary', pos_label=1)
-    f1_scores_label1.append(f1)
-    f1 = f1_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], average='binary', pos_label=1)
-    mask_f1_scores_label1.append(f1)
+    #f1 = f1_score(labels[data_set], [m <= thr for m in distances[data_set]], average='binary', pos_label=1)
+    #f1_scores_label1.append(f1)
+    #f1 = f1_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], average='binary', pos_label=1)
+    #mask_f1_scores_label1.append(f1)
 
-    f1 = f1_score(labels[data_set], [m <= thr for m in distances[data_set]], average='binary', pos_label=0)
-    f1_scores_label0.append(f1)
-    f1 = f1_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], average='binary', pos_label=0)
-    mask_f1_scores_label0.append(f1)
+    #f1 = f1_score(labels[data_set], [m <= thr for m in distances[data_set]], average='binary', pos_label=0)
+    #f1_scores_label0.append(f1)
+    #f1 = f1_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], average='binary', pos_label=0)
+    #mask_f1_scores_label0.append(f1)
 
-    rec = recall_score(labels[data_set], [m <= thr for m in distances[data_set]])
-    recall_scores.append(rec)
-    rec = recall_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]])
-    mask_recall_scores.append(rec)
+    #rec = recall_score(labels[data_set], [m <= thr for m in distances[data_set]])
+    #recall_scores.append(rec)
+    #rec = recall_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]])
+    #mask_recall_scores.append(rec)
+
+    rep = classification_report(labels[data_set], [m <= thr for m in distances[data_set]], output_dict=True)
+    f1_scores_label1.append(rep['1.0']['f1-score'])
+    recall_label1.append(rep['1.0']['recall'])
+    precision_label1.append(rep['1.0']['precision'])
+    f1_scores_label0.append(rep['0.0']['f1-score'])
+    recall_label0.append(rep['0.0']['recall'])
+    precision_label0.append(rep['0.0']['precision'])
+    rep = classification_report(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], output_dict=True)
+    mask_f1_scores_label1.append(rep['1.0']['f1-score'])
+    mask_recall_label1.append(rep['1.0']['recall'])
+    mask_precision_label1.append(rep['1.0']['precision'])
+    mask_f1_scores_label0.append(rep['0.0']['f1-score'])
+    mask_recall_label0.append(rep['0.0']['recall'])
+    mask_precision_label0.append(rep['0.0']['precision'])
+    #f1 = f1_score(labels[data_set], [m <= mask_thr for m in mask_distances[data_set]], average='binary', pos_label=1)
+    #mask_f1_scores_label1.append(f1)
     
 
-header = ['model'] + [f'{data_set}-{column}' for data_set in ['train', 'test.iov', 'test.oov', 'dev'] for column in ['spearman_corr', 'spearman_pvalue', 'pearson_corr', 'pearson_pvalue', 'f1_score', 'f1_scores_label1', 'f1_scores_label0', 'recall']] + ['thr']
+header = ['model', 'k_fold'] + [f'{data_set}-{column}' for data_set in ['train', 'test.iov', 'test.oov', 'dev'] for column in ['spearman_corr', 'spearman_pvalue', 'pearson_corr', 'pearson_pvalue', 'f1_score', 'f1_scores_label1', 'f1_scores_label0', 'recall_label1', 'recall_label0', 'precision_label1', 'precision_label0']] + ['thr']
 header = "\t".join(header)
 
 stats_file = "TRiC-stats.tsv"
@@ -143,8 +167,8 @@ if not Path(stats_file).is_file():
 else:
     lines = open(stats_file, mode='r',encoding='utf-8').readlines()
 
-lines.append(f'{model_name}\t' + "\t".join([f'{spearman_corr[i]}\t{spearman_pvalue[i]}\t{pearson_corr[i]}\t{pearson_pvalue[i]}\t{f1_scores[i]}\t{f1_scores_label0[i]}\t{f1_scores_label1[i]}\t{recall_scores[i]}' for i in range(4)]) + f'\t{thr}\n')
-lines.append(f'{model_name}_mask\t' + "\t".join([f'{mask_spearman_corr[i]}\t{mask_spearman_pvalue[i]}\t{mask_pearson_corr[i]}\t{mask_pearson_pvalue[i]}\t{mask_f1_scores[i]}\t{mask_f1_scores_label1[i]}\t{mask_f1_scores_label0[i]}\t{mask_recall_scores[i]}' for i in range(4)]) + f'\t{mask_thr}\n')
+lines.append(f'{model_name}\t{k_fold}\t' + "\t".join([f'{spearman_corr[i]}\t{spearman_pvalue[i]}\t{pearson_corr[i]}\t{pearson_pvalue[i]}\t{f1_scores[i]}\t{f1_scores_label1[i]}\t{f1_scores_label0[i]}\t{recall_label1[i]}\t{recall_label0[i]}\t{precision_label1[i]}\t{precision_label0[i]}' for i in range(4)]) + f'\t{thr}\n')
+lines.append(f'{model_name}_mask\t{k_fold}\t' + "\t".join([f'{mask_spearman_corr[i]}\t{mask_spearman_pvalue[i]}\t{mask_pearson_corr[i]}\t{mask_pearson_pvalue[i]}\t{mask_f1_scores[i]}\t{mask_f1_scores_label1[i]}\t{mask_f1_scores_label0[i]}\t{mask_recall_label1[i]}\t{mask_recall_label0[i]}\t{mask_precision_label1[i]}\t{mask_precision_label0[i]}' for i in range(4)]) + f'\t{mask_thr}\n')
 
 with open(stats_file, mode='w', encoding='utf-8') as f:
     f.writelines(lines)
